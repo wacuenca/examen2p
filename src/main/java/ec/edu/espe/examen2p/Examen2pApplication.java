f9941cbdcf9b5dbf1c0122cc1148e48137ded417
Wagner Cuenca <wacuenca@espe.edu.ec>
2025-07-09 14:49:27 -0500

    Initial commit

---
+++ src/main/java/ec/edu/espe/examen2p/Examen2pApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/examen2p/Examen2pApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
